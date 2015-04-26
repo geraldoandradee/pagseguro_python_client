b6273d03df2c28f7035295f205ae72b39475eba3
Geraldo Andrade <geraldo@geraldoandrade.com>
2015-04-26 20:15:16 -0300

    Added url redirect parameter

--- pagseguro_python_client/__init__.py
+++ pagseguro_python_client/__init__.py
@@ -5,4 +5,4 @@ import os
 ENDPOINT = os.getenv("PAGSEGURO_PYTHON_CLIENT_ENDPOINT", 'https://ws.pagseguro.uol.com.br/v2/checkout')
 ENCODING = os.getenv("PAGSEGURO_PYTHON_CLIENT_ENCODING", 'UTF-8')
 CONTENT_TYPE = 'application/x-www-form-urlencoded'
-
+REDIRECT_PAYMENT = 'https://pagseguro.uol.com.br/v2/checkout/payment.html?code=%s'
